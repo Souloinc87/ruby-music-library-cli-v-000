ec416efa384de8b9b9dc1bd069f79e53fbe7b33e
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-09 22:37:54 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,7 +21,7 @@ class MusicLibraryController
     end
   end
 
-  def list_songs(song_list)
+  def list_songs
     Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
       puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
